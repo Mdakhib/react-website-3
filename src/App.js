4ed8a301c3f2e11d6e04be037422aeb39f841b97
Mdakhib <mohamedakhib@gmail.com>
2021-06-04 16:56:33 +0530

    second commit files deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-// import logo from './logo.svg';
-import Card from "./components/card/card"
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Card />
+    <div>
+      
     </div>
   );
 }
