0813f7f1f4d1e07ceea78077fb24cb427032ac6b
Mdakhib <mohamedakhib@gmail.com>
2021-06-04 16:25:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import Card from "./components/card/card"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
+
+export default App;
