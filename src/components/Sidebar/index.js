667a8bf45eeb15c1b7979c91da8e92c0469e7df0
Mdakhib <mohamedakhib@gmail.com>
2021-06-27 19:22:17 +0530

    sidebar component created and styled

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SideBtnWrap,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarRoute,
+  SidebarLink,
+} from "./SidebarElement";
 
 function index() {
     return (
-        <div>
-            
-        </div>
-    )
+      <SidebarContainer>
+        <Icon>
+          <CloseIcon />
+        </Icon>
+        <SidebarWrapper>
+          <SidebarMenu>
+            <SidebarLink to="about">About</SidebarLink>
+            <SidebarLink to="discover">Discover</SidebarLink>
+            <SidebarLink to="services">Services</SidebarLink>
+            <SidebarLink to="signup">Sign Up</SidebarLink>
+          </SidebarMenu>
+          <SideBtnWrap>
+            <SidebarRoute to="/signin">Sign In</SidebarRoute>
+          </SideBtnWrap>
+        </SidebarWrapper>
+      </SidebarContainer>
+    );
 }
 
 export default index
