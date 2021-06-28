671c39a15b651c9f6ca94c1a425f69da4890c89e
Mdakhib <mohamedakhib@gmail.com>
2021-06-28 16:13:30 +0530

    added button styling by fetching component and data file in which it works on ternary based

--- src/components/InfoSection/InfoElement.js
+++ src/components/InfoSection/InfoElement.js
@@ -1,11 +1,11 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
 
 export const InfoContainer = styled.div`
   color: #fff;
   background: ${({ lightBg }) => (lightBg ? "#f9f9f9" : "#010606")};
 
   @media screen and (max-width: 768px) {
-    padding: 100px o;
+    padding: 100px 0;
   }
 `;
 
@@ -97,3 +97,5 @@ export const Img = styled.img`
   margin: 0 0 10px 0;
   padding-right: 0;
 `;
+
+
