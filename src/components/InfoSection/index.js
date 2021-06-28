790fa331da0a6f69c31923362ce973e9df5af246
Mdakhib <mohamedakhib@gmail.com>
2021-06-28 14:21:19 +0530

    data is fetching through props and writing in to boiler

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -15,25 +15,37 @@ import {
   Img,
 } from "./InfoElement";
 
-function InfoSection() {
+function InfoSection({
+  lightBg,
+  imgStart,
+  topLine,
+  headline,
+  id,
+  lightText,
+  darkText,
+  description,
+  buttonLabel,
+  img,
+  alt,
+}) {
   return (
     <>
-      <InfoContainer>
+      <InfoContainer lightBg={lightBg} id={id}>
         <InfoWrapper>
-          <InfoRow>
+          <InfoRow imgStart={imgStart}>
             <Column1>
               <TextWrapper>
-                <TopLine>TopLine</TopLine>
-                <Heading>Heading</Heading>
-                <Subtitle>Subtitle</Subtitle>
+                <TopLine>{topLine} </TopLine>
+                <Heading lightText={lightText}> {headline} </Heading>
+                <Subtitle darkText={darkText}>{description} </Subtitle>
                 <BtnWrap>
-                  <Button to="home">Button</Button>
+                  <Button to="home">{buttonLabel} </Button>
                 </BtnWrap>
               </TextWrapper>
             </Column1>
             <Column2>
               <ImgWrap>
-                <Img />
+                <Img src={img} alt={alt} />
               </ImgWrap>
             </Column2>
           </InfoRow>
