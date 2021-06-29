aae5acc94832f6bc5ae7b61e61905e88f7fd9ada
Mdakhib <mohamedakhib@gmail.com>
2021-06-29 11:42:23 +0530

    footer  section is done with stylting and added social icons

---
+++ src/components/Footer/index.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinkContainer,
+  FooterLinkWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  WebsiteRights,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialIcons,
+  SocialIconLink,
+  SocialLogo,
+} from "./FooterElement";
+
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa";
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinkContainer>
+          <FooterLinkWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About us</FooterLinkTitle>
+              <FooterLink to="/signin">How it works</FooterLink>
+              <FooterLink to="/signin">Testinomials</FooterLink>
+              <FooterLink to="/signin">Careers</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Terms of services</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Videos</FooterLinkTitle>
+              <FooterLink to="/signin">Submit Video</FooterLink>
+              <FooterLink to="/signin">Ambassadors</FooterLink>
+              <FooterLink to="/signin">Agency</FooterLink>
+              <FooterLink to="/signin">Influencer</FooterLink>
+            </FooterLinkItems>
+          </FooterLinkWrapper>
+          <FooterLinkWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact us</FooterLinkTitle>
+              <FooterLink to="/signin">Contact</FooterLink>
+              <FooterLink to="/signin">Support</FooterLink>
+              <FooterLink to="/signin">Destinations</FooterLink>
+              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink to="/signin">Sponsorships</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to="/signin">Instagram</FooterLink>
+              <FooterLink to="/signin">Facebook</FooterLink>
+              <FooterLink to="/signin">Youtube</FooterLink>
+              <FooterLink to="/signin">Twitter</FooterLink>
+              <FooterLink to="/signin">ClubHouse</FooterLink>
+            </FooterLinkItems>
+          </FooterLinkWrapper>
+        </FooterLinkContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/">dolla</SocialLogo>
+            <WebsiteRights>
+              dolla © {new Date().getFullYear()} All rights reserved{" "}
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href="/" target="_blank" aria-label="Facebook">
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="YouTube">
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Linkedin">
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HeroSection from "../components/HeroSection";
 import InfoSection from "../components/InfoSection";
 import { homeObjOne, homeObjThree, homeObjTwo } from "../components/InfoSection/Data";
 import Services from "../components/Services";
+import Footer from "../components/Footer";
 
 function Home() {
   const [isOpen, setIsOpen] = useState(false);
@@ -22,6 +23,7 @@ function Home() {
       <InfoSection {...homeObjTwo} />
       <Services />
       <InfoSection {...homeObjThree} />
+      <Footer />
     </div>
   );
 }
